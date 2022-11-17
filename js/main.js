90ece193f85fac79ac32c08a547ee047ae9d1385
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2022-11-17 13:13:47 +0300

    bugfix for deletedNotes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let database = [];
 
+let deletedNotes = [];
+
+
 /**
  * 1. Main thread starting point
  */
@@ -148,6 +151,93 @@ function hideButton() {
 }
 
 
+function renderTrash() {
+    loadTrash();
+    createTrashTemplate();
+}
 
 
+function createTrashTemplate() {
+    let trashbin = document.getElementById('trash');
+    trashbin.innerHTML = '';
+    if (deletedNotes != null) {
+        for (i = 0; i < deletedNotes.length; i++) {
+            let note = document.createElement('div');
+            note.setAttribute("id", `deletedNote${i}`);
+            note.innerHTML += htmlTemplateDeleted(i);
+            trashbin.appendChild(note);
+            writeTrashInput(i);
+        }
+    }
+}
+
+
+function htmlTemplateDeleted(index) {
+    return `
+        <div id="containerRemoved${index}" style="min-height: 120px;">
+            <!--Bootstrap card-->
+            <div id="cardRemoved${index}" class="card" style="width: 23rem; background: #ced4da">
+                <div class="card-body">
+                    <!--Bootstrap input text-->
+                    <div class="input-group">
+                        <input id="titleRemoved${index}" type="text" class="form-control" placeholder="Title">
+                    </div>
+                    <!--Bootstrap input textarea-->
+                    <div class="input-group mb-3">
+                        <textarea id="textareaRemoved${index}" type="textarea" class="form-control"></textarea>
+                    </div>
+                    <!--Bootstrap input dropdown button-->
+                    <div class="btn-group">
+                        <button id="deleteRemoved${index}" type="button" class="btn btn-danger" onclick="deleteRemoved(${index})">Delete</button>
+                        <button id="restoreRemoved${index}" type="button" class="btn btn-primary" onclick="restoreRemoved(${index})">Restore</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+
+function writeTrashInput(index) {
+    let title = document.getElementById(`titleRemoved${index}`);
+    let text = document.getElementById(`textareaRemoved${index}`);
+    title.value += deletedNotes[index].title;
+    text.value += deletedNotes[index].text;
+}
+
+
+function saveTrash(index) {
+    let deletedNote = database[index];
+    console.log("setting deleted note: ", deletedNote);
+    deletedNotes.push(deletedNote);
+    console.log("pushing deleted note: ", deletedNotes);
+    let savedData = JSON.stringify(deletedNotes);
+    localStorage.setItem('deletedData', savedData);
+}
+
+
+function loadTrash() {
+    let loadedData = localStorage.getItem('deletedData');
+    if (loadedData !=null) {
+        deletedNotes = JSON.parse(loadedData);
+    }
+}
+
+
+function showNotes() {
+    let canvas = document.getElementById('canvas');
+    canvas.classList.remove('d-none');
+    let trash = document.getElementById('trash');
+    trash.classList.add('d-none');
+    render();
+}
+
+
+function showTrash() {
+    let canvas = document.getElementById('canvas');
+    canvas.classList.add('d-none');
+    let trash = document.getElementById('trash');
+    trash.classList.remove('d-none');
+    renderTrash();
+}
 
